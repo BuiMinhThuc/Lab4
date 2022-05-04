fb8e17295bc4d3c33717be4bcb4584328bb94533
BuiMinhThuc <buiminhthucvv2002@gmail.com>
2022-05-04 08:49:07 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+package Bai10;
+import Bai10.HocVien;
+
+public class Main {
+    public static void main(String[] args) {
+        String lct;
+       HocvienDH DH=new HocvienDH(0);
+       HocvienLT LT=new HocvienLT(0);
+       DH.nhapthongtin(lct);
+       if(lct="DH"){
+             DH.hocPhi();
+            DH.inthongtin();
+       }
+       else if(lct="LT"){
+            LT.hocPhi();
+            LT.inthongtin();
+       }
+       else
+            System.out.print("\nLoai chuong trinh khong hop le !"); 
+    }
+}
